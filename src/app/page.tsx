d0e2578ed028e1cc77e58fde426906fbab9da446
Mario <cerdamario13@gmail.com>
2023-06-06 21:18:08 -0500

    package updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,34 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+// import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+// import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import { Button } from '@mui/material';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function Home() {
+  
+  //getting data from localHost Python flask
+  const getAllRuns = async () => {
+    const url = new URL("http://127.0.0.1:5000/allRuns");
+    
+    const response = await fetch(url.toString());
+    const data = await response.json();
+    return data;
+  }
+  
   return (
-    <p>Hello World</p>
+    <>
+      <Button variant="contained" onClick={() => {getAllRuns()}}>GET DATA</Button>
+      Hello world!
+    </>
   )
 }
